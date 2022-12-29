b01170795cbd9828865d63e11e065b1ab2715d17
singhkovid <kovid.singh22@gmail.com>
2022-12-30 03:45:47 +0530

    first commit

---
+++ src/com/CharNode.java
@@ -0,0 +1,15 @@
+package com;
+
+public class CharNode {
+    CharNode next;
+    char ch;
+    String bit_size;
+    int frequency = 0;
+
+    CharNode(final char ch, final String code, final int frequency) {
+        this.ch = ch;
+        this.bit_size = code;
+        this.frequency = frequency;
+        next = null;
+    }
+}
